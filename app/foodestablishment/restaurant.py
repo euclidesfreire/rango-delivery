ace124a5d89c6580ccd2c56e52e77e87a8ecaf86
EUCLIDES FREIRE DA SILVA ABREU <euclides.abreu@equatorialenergia.com.br>
2025-06-13 11:30:50 -0300

    feat: add class Restaurant

---
+++ app/foodestablishment/restaurant.py
@@ -0,0 +1,8 @@
+from foodestablishment import FoodEstablishment
+
+class Restaurant(FoodEstablishment):
+    
+    def __init__(self, name, category):
+        super.__init__(name, category)
+    
+    
