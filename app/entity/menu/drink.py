4f0d491555f3181f4779ef0ce5c43ab21c716ec2
EUCLIDES FREIRE DA SILVA ABREU <euclides.abreu@equatorialenergia.com.br>
2025-06-20 18:06:58 -0300

    refactor: menu

--- app/entity/menu/drink.py
+++ app/entity/menu/drink.py
@@ -1,4 +1,12 @@
 from entity.menu.item import Item
 
 class Drink(Item):
-    pass
+    _name: str
+    _price: float
+    _descricao: str
+    _category: str
+
+    def __init__(self, name, price, category):
+        self._name = name
+        self._price = price
+        self._category = category
--- app/entity/menu/food.py
+++ app/entity/menu/food.py
@@ -1,4 +1,9 @@
 from entity.menu.item from Item
 
 class Food(Item):
-    pass
+    _descricao: str
+
+    def __init__(self, name, price):
+        self._name = name
+        self._price = price
+        self._category = category
